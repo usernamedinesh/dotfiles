356935cf59491deca1447ebd46d32c162b906df5
usenamedinesh <borod9200@gmail.com>
2024-05-08 14:21:07 +0530

    rm script

--- cht.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prompt the user for a session name
-echo -n "Session name: "
-read session_name
-
-# Create a new TMUX session with the provided name and launch Neovim
-tmux new-session -s "$session_name" -d "nvim ."
-tmux attach-session -t "$session_name"
