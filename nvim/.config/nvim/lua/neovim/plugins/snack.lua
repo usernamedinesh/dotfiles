bd3ea18620d2d1dc9f15a6f3eaf9a9841f58c2e3
usernamedinesh <borod9200@gmail.com>
2025-02-17 09:38:11 +0530

    add session.sh snack.lua

---
+++ nvim/.config/nvim/lua/neovim/plugins/snack.lua
@@ -0,0 +1,133 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            bigfile = { enabled = true },
+            picker = {
+                matcher = {
+                    frecency = true,
+                },
+            },
+            notifier = {
+                enabled = false,
+                top_down = false, -- place notifications from top to bottom
+            },
+        },
+
+        keys = {
+
+            { "<leader>,",  function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
+            { "<leader>/",  function() Snacks.picker.grep() end,                                    desc = "Grep" },
+            { "<leader>:",  function() Snacks.picker.command_history() end,                         desc = "Command History" },
+            { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+            { "<leader>fg", function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
+            { "<leader>fr", function() Snacks.picker.recent() end,                                  desc = "Recent" },
+
+            { "<leader>gb", function() Snacks.picker.git_branches({ layout = "select" }) end,       desc = "Git Branches" },
+            { "<leader>gl", function() Snacks.picker.git_log() end,                                 desc = "Git Log" },
+            { "<leader>gs", function() Snacks.picker.git_status() end,                              desc = "Git Status" },
+            -- { "<leader>gS", function() Snacks.picker.git_stash() end,                               desc = "Git Stash" },
+            -- { "<leader>gd", function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
+            { "<leader>gf", function() Snacks.picker.git_log_file() end,                            desc = "Git Log File" },
+
+            { "<leader>sb", function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
+            -- { "<leader>sB", function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
+            { "<leader>sw", function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word", mode = { "n", "x" } },
+
+            -- search
+            { '<leader>s"', function() Snacks.picker.registers() end,                               desc = "Registers" },
+            { '<leader>s/', function() Snacks.picker.search_history() end,                          desc = "Search History" },
+            { "<leader>sa", function() Snacks.picker.autocmds() end,                                desc = "Autocmds" },
+            { "<leader>sb", function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
+            { "<leader>sc", function() Snacks.picker.command_history() end,                         desc = "Command History" },
+            { "<leader>sC", function() Snacks.picker.commands() end,                                desc = "Commands" },
+            { "<leader>sh", function() Snacks.picker.help() end,                                    desc = "Help Pages" },
+            { "<leader>sH", function() Snacks.picker.highlights() end,                              desc = "Highlights" },
+            { "<leader>si", function() Snacks.picker.icons() end,                                   desc = "Icons" },
+            { "<leader>sj", function() Snacks.picker.jumps() end,                                   desc = "Jumps" },
+            { "<leader>sk", function() Snacks.picker.keymaps() end,                                 desc = "Keymaps" },
+            { "<leader>su", function() Snacks.picker.undo() end,                                    desc = "Undo History" },
+            { "<leader>uC", function() Snacks.picker.colorschemes() end,                            desc = "Colorschemes" },
+
+            -- LSP
+            { "gd",         function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
+            { "gD",         function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
+            { "gr",         function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+            { "gI",         function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
+            { "gy",         function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
+            { "<leader>ss", function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
+            { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
+
+            -- Other
+            { "<leader>.",  function() Snacks.scratch() end,                                        desc = "Toggle Scratch Buffer" },
+            { "<leader>S",  function() Snacks.scratch.select() end,                                 desc = "Select Scratch Buffer" },
+            { "<leader>cR", function() Snacks.rename.rename_file() end,                             desc = "Rename File" },
+            { "<leader>gB", function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
+            {
+                "<leader>gl",
+                function()
+                    Snacks.picker.git_log({
+                        finder = "git_log",
+                        format = "git_log",
+                        preview = "git_show",
+                        confirm = "git_checkout",
+                        -- layout = "vertical",
+                    })
+                end,
+                desc = "Git Log",
+            },
+            {
+                "<leader>km",
+                function()
+                    Snacks.picker.keymaps({
+                        layout = "vertical",
+                    })
+                end,
+                desc = "Keymaps",
+            },
+            -- File picker
+            {
+                "<leader>ff",
+                function()
+                    Snacks.picker.files({
+                        finder = "files",
+                        format = "file",
+                        show_empty = true,
+                        supports_live = true,
+                        -- layout = "vscode"
+                    })
+                end,
+                desc = "Find Files",
+            },
+            -- Navigate my buffers
+            {
+                "<leader>fb",
+                function()
+                    Snacks.picker.buffers({
+                        -- I always want my buffers picker to start in normal mode
+                        on_show = function()
+                            vim.cmd.stopinsert()
+                        end,
+                        finder = "buffers",
+                        format = "buffer",
+                        hidden = false,
+                        unloaded = true,
+                        current = true,
+                        layout = "vscode",
+                        sort_lastused = true,
+                        win = {
+                            input = {
+                                keys = {
+                                    ["d"] = "bufdelete",
+                                },
+                            },
+                            list = { keys = { ["d"] = "bufdelete" } },
+                        },
+                        -- In case you want to override the layout for this keymap
+                    })
+                end,
+                desc = "[P]Snacks picker buffers",
+            },
+        },
+    },
+}
---
+++ script/script/session.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Get the current username
+USERNAME=$(whoami)
+
+# Define session names
+DEFAULT_SESSION="${USERNAME}-home"
+SESSION_PREFIX="term"
+
+# Function to check if a session exists
+session_exists() {
+    tmux has-session -t "$1" 2>/dev/null
+}
+
+# If no session exists, start with DEFAULT_SESSION
+if ! session_exists "$DEFAULT_SESSION"; then
+    tmux new-session -s "$DEFAULT_SESSION"
+    exit 0
+fi
+
+# Try creating term2, term3, term4...
+for i in {2..4}; do
+    NEW_SESSION="${SESSION_PREFIX}${i}"
+    if ! session_exists "$NEW_SESSION"; then
+        tmux new-session -s "$NEW_SESSION"
+        exit 0
+    fi
+done
+
+echo "Error: Maximum number of sessions (home, term2, term3, term4) reached!"
+exit 1
+
